0e0d9ba13fdca09bbe52893d2b4b66348072e753
oiler <jrf1039@gmail.com>
2018-06-18 18:21:51 -0400

    add shortcut to single file upload

--- upload.sh
+++ upload.sh
@@ -5,3 +5,5 @@ do
   echo "Uploading $f file..."
   aws s3 cp $f s3://sps.publicsport.net/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 done
+
+#aws s3 cp player.json s3://sps.publicsport.net/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
